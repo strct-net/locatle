7dd5592e520ad8310b94122dd3fee7e2eddfb21c
PaddiM8 <bakk@tuta.io>
2022-09-07 00:24:21 +0200

    Even out distribution

--- data/remove_country.py
+++ data/remove_country.py
@@ -4,6 +4,7 @@ import sys
 
 country = sys.argv[1]
 after_id = int(sys.argv[2])
+chance_remove = int(sys.argv[3])
 
 with open("image_dict.json", "r") as dict_r:
     image_dict = json.load(dict_r)
@@ -14,7 +15,7 @@ with open("image_list.json", "r") as list_r:
 new_image_dict = {}
 new_image_list = []
 for i, item in enumerate(image_list):
-    if item["country"] != country or i <= after_id or random.randint(1, 20) == 1:
+    if item["country"] != country or i <= after_id or random.randint(1, 100) > chance_remove:
         new_image_list.append(image_list[i])
         new_image_dict[item["id"]] = image_dict[item["id"]]
 
